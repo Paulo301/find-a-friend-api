b1bfb37f0675d9e714376d58576464cdb541a32c
Paulo Victor Lemos de Almeida <paulolemos.dev@gmail.com>
2024-05-28 15:24:02 -0300

    feat: create docker compose

---
+++ prisma/migrations/20240528182205_create_organizations_and_pets/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "responsible_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "zip_code" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "energy_level" TEXT NOT NULL,
+    "independence_level" TEXT NOT NULL,
+    "environment" TEXT NOT NULL,
+    "photos" TEXT[],
+    "requirements" TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "organization_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_email_key" ON "organizations"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
